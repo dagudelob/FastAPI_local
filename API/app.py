fd12456aa08e35e5f3130791cd5951fbbfc6cca1
David Agudelo <agudelo.david@hotmail.com>
2025-04-10 10:39:21 -0400

    instructions to run the FastAPI

--- API/app.py
+++ API/app.py
@@ -14,3 +14,20 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+ ''' 
+ INSTRUCTIONS TO RUN THE FASTAPI APP:
+     
+    # 1. Install FastAPI and Uvicorn if you haven't already:
+ move to the directory where the app.py file is located and run the command below in the terminal:
+> cd API
+    
+    
+    #2. Create a virtual environment and activate it: 
+# To run the FastAPI app, use the command below in the terminal:
+  uvicorn app:app --reload
+ 
+ 
+    #3 Open google chrome and go to url:  http://127.0.0.1:8000/familia
+ 
+ '''
