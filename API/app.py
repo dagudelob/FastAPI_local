4401f4389fc381be68a0e63028dc5d4cc32da7bf
David Agudelo <agudelo.david@hotmail.com>
2025-04-10 12:41:44 -0400

    API working

--- API/app.py
+++ API/app.py
@@ -19,15 +19,17 @@ def get_superheroes():
  INSTRUCTIONS TO RUN THE FASTAPI APP:
      
     # 1. Install FastAPI and Uvicorn if you haven't already:
- move to the directory where the app.py file is located and run the command below in the terminal:
-> cd API
-    
+    pip install fastapi uvicorn
     
     #2. Create a virtual environment and activate it: 
-# To run the FastAPI app, use the command below in the terminal:
-  uvicorn app:app --reload
+    python -m venv venv
+    
+    #3. Move to the directory where the app.py file is located and run the command below in the terminal:
+    > cd API
+    
+    #4. To run the FastAPI app, use the command below in the terminal:
+    uvicorn app:app --reload
  
- 
-    #3 Open google chrome and go to url:  http://127.0.0.1:8000/familia
+    #5. Open google chrome and go to url:  http://127.0.0.1:8000/familia
  
  '''
